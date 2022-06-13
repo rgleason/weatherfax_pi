96b4195698796727bab2a705a8cd38ed25fd3129
Alec Leamas <leamas.alec@nowhere.net>
2022-06-13 08:33:38 +0200

    Templates: Updating cmake

--- cmake/ConfigureManifest.cmake
+++ cmake/ConfigureManifest.cmake
@@ -1,16 +1,21 @@
-# Given a Flatpak manifest path, configure a new manifest in CMAKE_BINARY_DIR. 
-#
+# Given a Flatpak manifest path, configure a new manifest in CMAKE_BINARY_DIR.
 # Return path to the configured manifest in ${new_manifest_path}
 #
-# When configuring, process the tokens @plugin_name and @include.
+# When configuring, process the tokens @plugin_name, @app_id and @include.
 #
-# @plugin_name is replaced with the PLUGIN_API_NAME value from Plugin.cmake.
+# @plugin_name is replaced with the PLUGIN_API_NAME value from Plugin.cmake,
+# converted to lowercase. This is expected to be used in the id: line,
+# allowing for generic manifests without dependencies.
+#
+# @app_id is replaced with the base part of the id: line, possibly after
+# substituting @plugin_name. The part after the last dot is used. This is
+# used as installation prefix in generic library snippets .
 #
 # @include are expected in lines like:
 #
 #     - @include libs/glu.yaml
 #
-# Copy the contents of the filename mentioned after @include into the new 
+# Copy the contents of the filename mentioned after @include into the new
 # manifest.
 #  - Filename is relative to the project top-level directory.
 #  - The indentation of the '-' char is added to each line in the included
@@ -18,7 +23,7 @@
 
 function(configure_manifest manifest new_manifest_path)
 
-  # Compute generated manifest path and return in ${new_manifest_path}
+  # Compute generated manifest path and return it in ${new_manifest_path}
   #
   if (${CMAKE_VERSION} VERSION_LESS 3.20)
     get_filename_component(manifest_basename ${manifest} NAME)
@@ -31,6 +36,18 @@ function(configure_manifest manifest new_manifest_path)
   if (EXISTS ${new_manifest})
     return ()
   endif ()
+
+  # Determine the overall plugin application id app_id
+  #
+  string(TOLOWER "${PACKAGE_NAME}" pkg_lc_name)
+  file(STRINGS ${manifest} app_id REGEX "^id: ")
+  string(REPLACE @plugin_name ${pkg_lc_name} app_id ${app_id})
+  string(REGEX REPLACE ".*[.]"  "" app_id "${app_id}")
+  if ("${app_id}" STREQUAL "")
+    message(FATAL_ERROR "Cannot find a proper id: line in ${manifest}")
+  endif ()
+  message(STATUS "Using app_id: ${app_id}")
+
 
   # Process @include
   #
@@ -50,15 +67,16 @@ function(configure_manifest manifest new_manifest_path)
     endif ()
   endforeach ()
 
-  # Process @plugin_name and drop comments
+  # Process @plugin_name and  @app_id. Drop comments.
   #
   file(STRINGS ${new_manifest} lines)
-  file(WRITE ${new_manifest} "")
+  file(REMOVE ${new_manifest})
   foreach (line ${lines})
-    if ("${line}" MATCHES "[ \t]*\#") 
+    if ("${line}" MATCHES "[ \t]*\#")
       continue ()
     endif ()
-    string(REPLACE @plugin_name ${PLUGIN_API_NAME} line "${line}")
+    string(REPLACE @plugin_name ${pkg_lc_name} line ${line})
+    string(REPLACE @app_id ${app_id} line "${line}")
     file(APPEND ${new_manifest} "${line}\n")
   endforeach ()
 endfunction ()
